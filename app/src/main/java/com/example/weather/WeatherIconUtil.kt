7efe021ca05ebe02db8937f0eb56be1c81fb0bb5
Charlie_Yang <Charlie_Yang@corpnet.asus>
2020-11-04 01:59:57 +0800

    add weather icon in cardview

---
+++ app/src/main/java/com/example/weather/WeatherIconUtil.kt
@@ -0,0 +1,22 @@
+package com.example.weather
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+
+object WeatherIconUtil {
+    fun getIconFromWeather(context: Context, weather: String): Drawable? {
+        when (weather) {
+            context.getString(R.string.sunny)
+            -> return context.getDrawable(R.drawable.sun)
+            context.getString(R.string.mostly_clear), context.getString(R.string.partly_clear)
+            -> return context.getDrawable(R.drawable.sun_cloud)
+            context.getString(R.string.cloudy), context.getString(R.string.mostly_cloudy),
+            context.getString(R.string.mostly_cloudy_1), context.getString(R.string.partly_cloudy)
+            -> return context.getDrawable(R.drawable.cloud)
+            context.getString(R.string.cloud_rain)
+            -> return context.getDrawable(R.drawable.rain)
+        }
+        return null
+    }
+
+}
